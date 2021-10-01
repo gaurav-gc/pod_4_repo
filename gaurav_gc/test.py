fb8bd6215102423a981b2661585bb85077e50773
Brett Buskirk <buskirkbrett8@gmail.com>
2021-09-30 20:22:31 -0400

    added files

--- gaurav_gc/test.py
+++ gaurav_gc/test.py
@@ -1,105 +1,38 @@
-# Creating variables to store the current (approximate) market price of these 5 companies - Amazon, Apple, Facebook, Google and Microsoft.
-amazon = 3000
-apple = 100
-fb = 250
-google = 1400
-msft = 200
+# You run a startup media company called Ripple Media
+# It's typical when you hire a new employee in your company, to setup an email id for them
 
-stocks = {
-    'amzn': ['Amazon', amazon],
-    'appl': ['Apple', apple],
-    'fb': ['Facebook', fb],
-    'goog': ['Google', google],
-    'msft': ['Microsoft', msft]
-}
+print('Question 1')
+employee_name = 'Ash Rahman'
 
+# You have decided the format of the email should be: Ash Rahman -> ash.rahman@ripplemedia.com
+# Let's write some code that converts a name into an email id that matches this format
+# 1.1 TODO: Let's save the lowercase version of the employee_name in a new variable 'lower_name'
+lower_name = employee_name.lower()
+# 1.2 TODO: We want to separate the first name and last name and save it in a variable 'names_list'
+names_list = lower_name.split(" ")
+# 1.3 TODO: We want to join the first name and last name with a '.' and save it in a variable called 'joined_names'
+joined_names = ".".join(names_list)
+# 1.4 TODO: We want to add '@ripplemedia.com' to the end of the string inside joined_names and save it in a variable 'email'
+email = joined_names + '@ripplemedia.com'
+print(email)
 
-stock_amt = 0
-stock_details = []
-u_name = ""
-u_stock = ""
+print('Question 2')
 
+# Congratulations! Your team is expanding. Below is a list of their names:
+names = ['Max Bartlett', 'Angelita Norris', 'Stewart Mueller', 'Dominique Henry', 'Carmela Gross', 'Bettie Mcmillan', 'Sara Ellison', 'Ira Anthony', 'Pauline Riley', 'Ben Weber',
+         'Joanne Mcknight', 'Loren Gould', 'Jamar Singh', 'Amanda Vance', 'Tyrell Andrade', 'Jana Clements', 'Eddy Mcbride', 'Marsha Meyer', 'Elbert Shannon', 'Alyce Hull']
 
-def get_stock():
-    return input("\nWhich stock are you interested in? \
-    \nType 'amzn' for Amazon, 'appl' for Apple, 'fb' for Facebook, 'goog' for Google and 'msft' for Microsoft. \n")
+emails = []
 
+# We want to convert all their names into the same format from Question 1
+# 2.1 TODO: Use a "for" loop to go over each name in the names list
+# 2.2 TODO: Inside the "for" loop, create the email id by re-using the logic from Question 1 and...
+# 2.3 TODO: ..add the email to the emails list
+for name in names:
+    lower_name = name.lower()
+    names_list = lower_name.split(" ")
+    joined_names = ".".join(names_list)
+    email = joined_names + '@ripplemedia.com'
+    emails.append(email)
 
-def get_name():
-    return input("\nPlease enter your name: ")
-
-
-def get_savings():
-    return int(input("\nPlease enter the amount of savings you would like to work with: "))
-
-
-def evaluate(s):
-    for stock in stocks:
-        if s == stock:
-            return stocks[stock]
-    print("test")
-    evaluate(get_stock())
-
-
-def stock_purchase(sa, us):
-    if sa and us:
-        if int(us / sa):
-            return 0
-    return 1
-
-
-def report(name, savings, stock, cost):
-    print(f"{name} has {savings} in savings and he can buy {int(savings / int(cost))} of {stock} at the current price of {cost}.\n")
-
-
-print("Challenge 3.2: Playing with the stock market")
-
-print()
-
-print("Challenge 3.2.1: Taking user input")
-# Write code to ask the client his name and save it to a variable..
-u_name = get_name()
-# Write code to ask the client his savings and save it to another variable.
-u_savings = get_savings()
-
-if u_savings < apple:
-    print(f"You do not have enough money to invest.  Sorry {u_name}.\n")
-    exit()
-# Write code to ask the client the stock he is interested in and save it to another variable, as shown below.
-u_stock = get_stock()
-
-print()
-
-print("Challenge 3.2.2: Perform user-specific calculations")
-# You have all 3 user inputs stores in variables.
-# Based on that, write conditional (if-elif-else) statements to find out the number of stocks of the company that can be purchased with the savings amount.
-# assert(u_stock == 'amzn' or u_stock == 'appl' or u_stock == 'fb' or u_stock == 'goog' or u_stock == 'msft')
-# assert(u_savings > 0)
-
-'''
-Your code should look like this:
-if stock == "amzn":
-    ...
-elif ...
-else ...
-'''
-
-# More concise way
-
-stock_details = evaluate(u_stock)
-stock_amt = stock_details[1]
-while stock_purchase(stock_amt, u_savings):
-    print(
-        f"\nI'm sorry, {u_name}, you don't have enough to purchase the {stock_details[0]} stock.  Please choose another.\n")
-    stock_details = evaluate(get_stock())
-    stock_amt = stock_details[1]
-
-print()
-
-print("Challenge 3.2.3: Output for the user the result")
-# Once you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
-
-# Alex has $5000 in savings and he can buy 50 shares of Apple at the current price of $100.
-report(u_name, u_savings, stock_details[0], stock_amt)
-
-print()
+print(emails)
