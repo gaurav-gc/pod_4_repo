b4b5fc5010e0ad6b9d0edf5ae52a1e27cbf374e9
guptg729 <ga.guptacasale@gmail.com>
2021-10-03 10:22:31 -0400

    scrap files

--- gaurav_gc/restaurants_challenge.py
+++ gaurav_gc/restaurants_challenge.py
@@ -6,7 +6,6 @@ print("Question 1")
 
 '''
 Below is a dictionary consisting of details of 1 restaurant fetched from Yelp. 
-
 Go through the dictionary and print out the following 3 pieces of information about the restaurant: 
 1. The latitude and longitude of Four Barrel Coffee 
 2. The complete address of Four Barrel Coffee, formatted as a string - it should include the address, city, state and the zip code. 
@@ -32,79 +31,89 @@ restaurant = {
 
 # print(restaurant)
 
-# TODO: Write code to print the latitude and longitude of Four Barrel Coffee.
-print(f'({restaurant["longitude"]}) , ({restaurant["longitude"]})')
+print('latitude and longitude of Four Barrel Coffee:')
+longitude = restaurant.get("longitude")
+lat = restaurant.get("latitude")
+print(longitude)
+print(lat)
 
-# TODO: Write code to print the complete address of the Four Barrel Coffee, formatted as a string - it should include the address, city, state and the zip code.
-coffee_address = restaurant["address1"]
-coffee_city = restaurant["city"]
-coffee_state = restaurant["state"]
-coffee_zip = restaurant["zip_code"]
-print(f'{coffee_address}\n{coffee_city}, {coffee_state}\n{coffee_zip}')
+print('complete address of the Four Barrel Coffee, formatted as a string:')
+street = restaurant.get('address1')
+city = restaurant.get('city')
+state = restaurant.get('state')
+zip = restaurant.get('zip_code')
+print(street + " " + city + " " + state + " " + zip)
 
-# TODO: Write code to print the URL of the website of Four Barrel Coffee.
-
-print(restaurant["url"])
-
+print('URL of the website of Four Barrel Coffee:')
+url = restaurant.get('url')
+print(url)
 
 print()
-
 print("Question 2")
 
-# TODO: Choose 3 of your most favourite restaurants in NYC, and create a dictionary for each of them with the following key-value pairs:
-#         1. name : name of the resturant (string)
-#         2. address: address of the restaurant (string)
-#         3. favourite_dish: your favourite thing to order at the restaurant (string)
+print('Choose 3 of your most favourite restaurants in NYC, and create a dictionary for each of them')
+#  with the following key-value pairs:
+# #         1. name : name of the resturant (string)
+# #         2. address: address of the restaurant (string)
+# #         3. favourite_dish: your favourite thing to order at the restaurant (string)
 
-# TODO: Print each dictionary
-
-restuarant_1 = {
-    "name": "Pelicana Chicken",
-    "address": "11 W 32nd St #3819",
-    "favourite_dish": "boneless chicken"
+restaurant1 = {
+    "name": "Bodega",
+    "address": "any street corner, new york, ny 10001",
+    "favorite_dishes": "chopped cheese"
 }
-
-restuarant_2 = {
-    "name": "Good Chinese",
-    "address": "663 Knickerbocker Ave, Brooklyn, NY 11221",
-    "favourite_dish": "steamed dumplings"
+restaurant2 = {
+    "name": "bareburger",
+    "address": "123 main street brooklyn ny 11705",
+    "favorite_dishes": "chicken fingers"
 }
-
-restuarant_3 = {
-    "name": "Bricklyn Pizza",
-    "address": "1240 Hancock St, Brooklyn, NY 11221",
-    "favourite_dish": "calzone"
+restaurant3 = {
+    "name": "champion",
+    "addressurl": "100 delancy street new york, ny 11725",
+    "favorite_dishes": "dollar slice"
 }
+print('Print each dictionary')
+print(restaurant1)
+print(restaurant2)
+print(restaurant3)
 
-# The dictionary for each restaurant should look something like this
+# # The dictionary for each restaurant should look something like this
 
+# '''
+# restaurant_1  = {
+#     "name": "Subway",
+#     "address" : "116th & Broadway, NY 10016",
+#     "favourite_dish" : "Chicken BLT Sandwich" }
+# '''
 
 print()
 
 print("Question 3")
-'''
-Imagine that any 1 of your most favourite restaurants stopped serving your favourite dish there. 
-Remove the 'favourite_dish' key value pair from that restaurant's dictionary
-'''
+# '''
+# Imagine that any 1 of your most favourite restaurants stopped serving your favourite dish there.
+# Remove the 'favourite_dish' key value pair from that restaurant's dictionary
+# '''
 
-# TODO: Remove the 'favourite_dish' key-value pair from one of your 3 restaurants
-restuarant_1.pop('favourite_dish')
-
-# TODO: Print the new dictionary. The new dictionary should only contain 'name' and 'address' for that restaurant
-print(restuarant_1)
+print('Remove the favorite_dish key-value pair from one of your 3 restaurants')
+print('Print the new dictionary.')
+restaurant1.pop("favorite_dishes")
+restaurant2.pop("favorite_dishes")
+restaurant3.pop("favorite_dishes")
+print(restaurant1)
+print(restaurant2)
+print(restaurant3)
 print()
 
 print("Question 4")
-'''
-Imagine that another one of your most favourite restaurants modified its address. 
-Update just this value in that restaurant's dictionary
-'''
+# '''
+# Imagine that another one of your most favourite restaurants modified its address.
+# Update just this value in that restaurant's dictionary
+# '''
 
-# TODO: Update the address field of 1 restaurant
-restuarant_2["address"] = "40 Broad St"
-# TODO: Print the new address of the restaurant by accessing that field of the restaurant's dictionary
-print(restuarant_2["address"])
-# TODO: Print the updated dictionary.
-print(restuarant_2)
-
-print()
+print('Update the address field of 1 restaurant')
+print('Print the new address of the restaurant')
+print('Print the updated dictionary.')
+restaurant1["address"] = '1234 beach avenue'
+new_address = restaurant1.get("address")
+print(new_address)
+print(restaurant1)
