5a507f89a97a4de7d98e80a2840c1e7ca26a4653
llukkah <llukkah.rey@gmail.com>
2021-10-03 21:41:00 -0400

    added importing notes

---
+++ diana_delos_reyes/notes/07_importing.py
@@ -0,0 +1,14 @@
+NUMPY library
+
+avg = np.mean(monthly_plays)
+
+arrange
+
+
+Importing your own functions
+
+If file1 and File2 is in the same file path.  File_1 has a function, hellogoodbye(), you can import that function in File_2 by:
+
+from File_1 import hellogoodbye()
+or to import all functions to a File:
+from File_1 import *
