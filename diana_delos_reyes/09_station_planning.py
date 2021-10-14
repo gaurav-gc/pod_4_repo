ce96fbd1bc48316668a20c9f5fdce826b65c7833
llukkah <llukkah.rey@gmail.com>
2021-10-13 20:28:23 -0400

    added inheritance.py and 09_station_planning.py // completed challenges

---
+++ diana_delos_reyes/09_station_planning.py
@@ -0,0 +1,26 @@
+from inheritance import *
+
+print('')
+print('')
+print('')
+print('Bus Station 1')
+bus1 = BusStation(station_name="la playa", location="on the beach", routes=['tulum', 'cabo', 'mexico'], open=True)
+bus1.open_station()
+bus1.close_station()
+bus1.show_info()
+
+print('')
+print('')
+print('')
+print('Subway Station 1')
+tijuana = SubwayStation(station_name="market st", location = "4th street and market", lines=['A', 'B', 'C', 'D'])
+tijuana.show_info()
+
+print('')
+print('')
+print('')
+print('Subway station 2')
+frisco = SubwayStation(station_name="twin peaks", location = "soma", lines=['1', '2', '3', '4'])
+frisco.show_info()
+
+# when running this page, why is it running both inheritance.py and 09_station_planning.py ?
