544f2eacbc06717703348033ef5b9c546ddc6da0
llukkah <llukkah.rey@gmail.com>
2021-10-25 16:29:20 -0400

    added and completed sandwich challenge

--- diana_delos_reyes/django-projects/mysite/mysite/settings.py
+++ diana_delos_reyes/django-projects/mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello.apps.HelloConfig'
+    'hello.apps.HelloConfig',
+    'sandwich.apps.SandwichConfig'
 ]
 
 MIDDLEWARE = [
