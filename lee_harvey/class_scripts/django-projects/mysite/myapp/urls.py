4f2ea2dfd5c90b3696d4728db43260a210451862
Lee Harvey <337harveyWgmail.com>
2021-10-25 19:28:32 -0600

    added finished sandwich, myapp (both without the venv dir) and updated html files;

--- lee_harvey/class_scripts/django-projects/mysite/myapp/urls.py
+++ lee_harvey/class_scripts/django-projects/mysite/myapp/urls.py
@@ -4,9 +4,9 @@ from myapp.views import hello, goodbye
 urlpatterns = [
     # myapp/
     path('', hello, name='hello'),
-    path('hello', hello, name='hello'),
+    # path('hello', hello, name='hello'),
     path('goodbye', goodbye, name='goodbye'),
-    path('goodbye/<str:name>', goodbye, name='goodbye'),
+    path('goodbye/<str:name>', goodbye, name='goodbye_name'),
     # myapp/<str:name>
     path('<str:name>', hello, name='hello_name'),
 ]
