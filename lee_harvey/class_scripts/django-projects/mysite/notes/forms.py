5919a8e8fc78090a8256def48ebf7cf2fb3bd563
Lee Harvey <337harveyWgmail.com>
2021-11-02 12:31:42 -0600

    Modified look of Tasks and Notes pages, updated todo.views.py to include the ability to remove a task from complete and add to todo list.

--- lee_harvey/class_scripts/django-projects/mysite/notes/forms.py
+++ lee_harvey/class_scripts/django-projects/mysite/notes/forms.py
@@ -3,4 +3,4 @@ from django import forms
 from .models import Notes
 
 class NotesForm(forms.Form):
-    notes = forms.CharField(label = 'Add note', max_length = 255)
+    notes = forms.CharField(widget=forms.Textarea(attrs={"rows":4, "cols":60}), label = 'Note', max_length = 255)
