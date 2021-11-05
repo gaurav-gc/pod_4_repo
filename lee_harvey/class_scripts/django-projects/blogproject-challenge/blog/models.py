1be5381df1ceec0d0d8905c9d3d09ed97380b8f1
Lee Harvey <337harveyWgmail.com>
2021-11-05 12:30:31 -0600

    Completed blog challenge

---
+++ lee_harvey/class_scripts/django-projects/blogproject-challenge/blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+# Create your models here.
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 255)
+    
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key = True)
+    img_link = models.URLField()
+    title =  models.CharField(max_length = 255)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.title
+    
+    def comments(self):
+        return Comment.objects.filter(Post)
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key = True)
+    comment = models.TextField()
+    post = models.ForeignKey(Post, default = 1, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.comment
